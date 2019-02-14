267b4abb67010ae126e9124c5e0dec5c65cf5124
dmswl9898 <sejj0126@naver.com>
2019-02-15 02:44:00 +0900

    Create csButton.cs

---
+++ UNITY/Project/csButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class csButton : MonoBehaviour
+{
+   
+    public Text startGame;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startGame.text = "Start Game";// text를 start game으로 바꿈
+       // startGame.GetComponent<Text>().text = "Start Game";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
