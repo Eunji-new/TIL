7ac8a051bdc0a60cf133185b49b8833b47ea6ad1
dmswl9898 <sejj0126@naver.com>
2021-12-14 23:26:55 +0900

    Create K번째수.cs

---
+++ UNITY/C# 이론/알고리즘 문제 풀기/K번째수.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands) {
+        int[] answer = new int[] {};
+        //배열 자르기
+        //배열 정렬하기
+        //k번째 숫자 구하기
+        return answer;
+    }
+    
+    void GetArrayPart()
+    {
+
+    }
+
+    void SortArray()
+    {
+
+    }
+
+    void GetK()
+    {
+        
+    }
+
+}
