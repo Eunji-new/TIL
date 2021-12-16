e923e3592d25ea75f46f40331883a2c05e5235b9
dmswl9898 <sejj0126@naver.com>
2021-12-16 21:26:28 +0900

    update 모의고사

--- UNITY/C# 이론/알고리즘 문제 풀기/모의고사.cs
+++ UNITY/C# 이론/알고리즘 문제 풀기/모의고사.cs
@@ -9,43 +9,39 @@ public class Solution {
         int[] replay2 = new int[]{2,1,2,3,2,4,2,5};
         int[] replay3 = new int[]{3,3,1,1,2,2,4,4,5,5};
         int[] correctNum = new int[]{0,0,0};
-        for(int idx = 0; idx<answers.Length; idx++)
-        {   
+        for(int idx = 0; idx<answer.Length; i++)
+            System.Console(idx + "\n");
             //1번
-            if(answers[idx] == replay1[idx%replay1.Length]){
+            if(answer[idx] == replay1[idx%replay1.Length]){
                 correctNum[0]++;
             }
             //2번
-            if(answers[idx] == replay2[idx%replay2.Length]){
+            if(answer[idx] == replay2[idx%replay2.Length]){
                 correctNum[1]++;
             }
             //3번
-            if(answers[idx] == replay3[idx%replay3.Length]){
+            if(answer[idx] == replay3[idx%replay3.Length]){
                 correctNum[2]++;
             }
         }
-
         int min = correctNum[0];
         int max = 0;
         int maxIdx = 0;
         for(int i = 0; i<correctNum.Length; i++){
             if(min>correctNum[i])
-            {  
                 min = correctNum[i];
-            }
             if(max<=correctNum[i])
             { 
                 max = correctNum[i];
                 maxIdx = i+1;
             }
         }
-        answer = (answer?.Concat(new int[]{maxIdx})?? new int[]{maxIdx}).ToArray();
+        answer = (answer?.Concat(new int[]{maxIdx}))?? new int[]{maxIdx};
         //같은게 있는지 검사
         for(int i = 0; i<correctNum.Length; i++){
             if(i!=maxIdx-1 && correctNum[i] == max)
-                answer = answer.Concat(new int[]{i+1}).ToArray();
+                answer = answer.Concat(new int[]{i+1});
         }
-        Array.Sort(answer);
         return answer;
     }
 }
